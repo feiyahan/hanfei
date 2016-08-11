26b03bdf07dc3eb406a8eda52acec74a84be15ef
feiyahan <feiyahan@163.com>
2016-08-11 15:24:02 +0800

    删除.iml文件

--- jdjr-service/src/main/java/com/feiyahan/jdjr/service/impl/IndexServiceImpl.java
+++ jdjr-service/src/main/java/com/feiyahan/jdjr/service/impl/IndexServiceImpl.java
@@ -1,9 +1,12 @@
 package com.feiyahan.jdjr.service.impl;
 
 import com.feiyahan.jdjr.service.IndexService;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by hanfei7 on 2016/8/11.
  */
+@Service
 public class IndexServiceImpl implements IndexService {
+
 }
