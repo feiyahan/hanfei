f973f5d1100fa68309ea6a52c614cc00babd0c21
feiyahan <feiyahan@163.com>
2016-08-15 15:53:16 +0800

    提交dao和pojo模块

---
+++ jdjr-dao/src/main/java/com/feiyahan/jdjr/dao/UsersDao.java
@@ -0,0 +1,7 @@
+package com.feiyahan.jdjr.dao;
+
+/**
+ * Created by hanfei7 on 2016/8/12.
+ */
+public interface UsersDao {
+}
---
+++ jdjr-pojo/src/main/java/com/feiyahan/jdjr/pojo/UserInfo.java
@@ -0,0 +1,26 @@
+package com.feiyahan.jdjr.pojo;
+
+/**
+ * Created by hanfei7 on 2016/8/12.
+ */
+public class UserInfo {
+
+    private String username;//用户名
+    private String password;//密码
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
