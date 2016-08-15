801c97d40c8ae1351ac651cc71d00f1757cbf975
feiyahan <feiyahan@163.com>
2016-08-15 11:52:47 +0800

    没什么，就是想提交了

--- jdjr-service/src/main/java/com/feiyahan/jdjr/service/IndexService.java
+++ jdjr-service/src/main/java/com/feiyahan/jdjr/service/IndexService.java
@@ -4,4 +4,5 @@ package com.feiyahan.jdjr.service;
  * Created by hanfei7 on 2016/8/11.
  */
 public interface IndexService {
+    void printProperties();
 }
--- jdjr-service/src/main/java/com/feiyahan/jdjr/service/impl/IndexServiceImpl.java
+++ jdjr-service/src/main/java/com/feiyahan/jdjr/service/impl/IndexServiceImpl.java
@@ -1,6 +1,10 @@
 package com.feiyahan.jdjr.service.impl;
 
+import com.feiyahan.jdjr.pojo.UserInfo;
 import com.feiyahan.jdjr.service.IndexService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,5 +12,22 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class IndexServiceImpl implements IndexService {
+    private final static Logger logger= LoggerFactory.getLogger(IndexServiceImpl.class);
 
+    @Value("${redis.timeout}")
+    private int redisTimeout;
+
+    @Value("${user.bean}")
+    private String userInfo;
+    @Value("${flag.trueOrFalse}")
+    private boolean trueOrFalse;
+
+    @Override
+    public void printProperties() {
+        logger.info("redisTimeout={}",redisTimeout==2);
+        logger.info("userInfoJson={}",userInfo);
+        logger.info("flag.trueOrFalse={}",trueOrFalse && false);
+//        UserInfo userInfo = JSONObject.parseObject(java.lang.String.valueOf(userInfoJson), UserInfo.class);
+//        logger.info("username={},password={}",userInfo.getUsername(),userInfo.getPassword());
+    }
 }
